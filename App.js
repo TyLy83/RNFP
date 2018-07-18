54772bde441ac0c461d8b242f5500a4301a59af5
Ty Ly <ty.ly81@gmail.com>
2018-07-18 21:40:34 +1200

    install react-navigation

--- App.js
+++ App.js
@@ -12,34 +12,37 @@ import {
   View
 } from 'react-native';
 import SplashScreen from 'react-native-splash-screen';
+import LoginScreen from './login.screen';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+// const instructions = Platform.select({
+//   ios: 'Press Cmd+R to reload,\n' +
+//     'Cmd+D or shake for dev menu',
+//   android: 'Double tap R on your keyboard to reload,\n' +
+//     'Shake or press menu button for dev menu',
+// });
 
 
 export default class App extends Component {
 
   componentDidMount() {
-    SplashScreen.hide()
+    //SplashScreen.hide()
+    console.log(`App.js : componentDidMount()`)
   }
 
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
-      </View>
+      // <View style={styles.container}>
+      //   <Text style={styles.welcome}>
+      //     Welcome to React Native!
+      //   </Text>
+      //   <Text style={styles.instructions}>
+      //     To get started, edit App.js
+      //   </Text>
+      //   <Text style={styles.instructions}>
+      //     {instructions}
+      //   </Text>
+      // </View>
+      <LoginScreen />
     );
   }
 }
